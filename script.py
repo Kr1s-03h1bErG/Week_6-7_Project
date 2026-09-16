676a3fddd55ca69d603ffbe9750dc327475a4f68
luchinofan22 <s1038294@student.knoxschools.org>
2026-09-16 15:46:19 +0000

    i just made some BULL;LLLLLLSHITITTTITTTT

--- script.py
+++ script.py
@@ -1,5 +1,11 @@
+#dictionary
+#usernames as values orkeys?
+
+games = {
+    #hear me out; 'examplegame' : {'creator' : 'exampleuser', 'description' : 'words', }
+}
 #master function ooopooioioooooioiuuoi
-def whoa()
+def system()
 
 #login BULLSHITTTTTTTTT
 def login(username)
@@ -8,3 +14,7 @@ def login(username)
 def your_games(username)
 
 #games?
+def post_game(game_name, description, price, buycount, username) #does username need to be an argument here
+
+def delete_game(game_name, username)
+
