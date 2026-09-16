308920786bdf6d23d76226de4529e1a451468395
luchinofan22 <s1038294@student.knoxschools.org>
2026-09-16 15:55:11 +0000

    iuno early next banner trust

--- script.py
+++ script.py
@@ -2,7 +2,7 @@
 #usernames as values orkeys?
 
 games = {
-    #hear me out; 'examplegame' : {'creator' : 'exampleuser', 'description' : 'words', }
+    #hear me out; 'examplegame' : {'creator' : 'exampleuser', 'description' : 'words', 'buycount' : number, 'price' : number} I JUST MAE SOME BULLLSHITTTTTT
 }
 #master function ooopooioioooooioiuuoi
 def system()
@@ -14,7 +14,14 @@ def login(username)
 def your_games(username)
 
 #games?
-def post_game(game_name, description, price, buycount, username) #does username need to be an argument here
+
+#crud functions woah
+#we don't need more information for delete update and view right? it can just get information from the dictionary
+def post_game(game_name, description, price, buycount, username) #does username need to be an argument here or will it automatically take it from the login thing or smth
 
 def delete_game(game_name, username)
+
+def update_game(game_name, username)
+
+def view_game(game_name, username) #username to enable/disable delete and update? #will delete and update be accessible on their own or only through view
 
