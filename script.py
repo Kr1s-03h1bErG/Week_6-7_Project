420e49601291e5fe914db2b1f59b0af0693cb18d
luchinofan22 <s1038294@student.knoxschools.org>
2026-09-22 18:24:31 +0000

    WE like rich

--- script.py
+++ script.py
@@ -1,5 +1,7 @@
 from crud_user import get_response, get_player_data, is_user_in_user, login, register
 from curd_game import add_game, read_game
+from rich import print as rprint, pretty
+from rich.panel import Panel
 
 #master function ooopooioioooooioiuuoi
 def system():
@@ -11,31 +13,24 @@ def system():
         else: 
             register()
     #prints username + games owned
-    print(login_stats)
+    print(Panel([darkblue]login_stats))
     player_data = get_player_data(login_stats[1])
     print(player_data)
     #crud uses wow
     choice = input('Would you like to: \n1) Delete one of your own games\n2) Post a new game\n3) Update the information for one of your games\n4) View available games\n')
     #delete
     if choice == 1:
-        gamename = input('What game do you want to delete?')
-        delete_game(gamename, login_stats[1])
+        delete_game(login_stats[1])
     #create
     elif choice == 2:
-        name = input('What is the name of the game you want to post? ')
-        genre = input('What genre is the game? ')
-        price = input('How much does the game cost? (If free enter 0) ')
-        add_game(name, genre, login_stats[1], price)
+        post_game(login_stats[1])
     #update
     elif choice == 3:
-        name = input('What is the name of the game you want to update? ')
-        #update_game(name, username)
+        update_game(login_stats[1])
     #read
     elif choice == 4:
         name = input('What game do you want to see the information for? ')
         view_game(name)
-
-
 
 #crud functions woah
 def post_game(username): 
@@ -46,10 +41,9 @@ def post_game(username):
 
 # def delete_game(game_name, username):
 
-
-# def update_game(game_name, username):
+def update_game(game_name, username):
+    pass
     
-
 def view_game(game_name): #username to enable/disable delete and update
     game_data = read_game(game_name)
     # print(game_data) # this is just the games data idk what else to say about it 
